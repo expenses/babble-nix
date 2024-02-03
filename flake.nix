963c35463920d9e2bf9fd71f1aeb65c0d3113be6
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-02-03 20:29:02 +1300

    Cross compiling

--- flake.nix
+++ flake.nix
@@ -1,27 +1,26 @@
 {
-  description = "My lib";
+  description = "C++ Crosscompilation Example";
 
-  outputs = { self, nixpkgs }: {
-
-    defaultPackage.x86_64-linux =
-    with import nixpkgs { system = "x86_64-linux"; };
-    stdenv.mkDerivation {
-    name = "babble";
-
-    # good source filtering is important for caching of builds.
-    # It's easier when subprojects have their own distinct subfolders.
-    src = fetchGit {
-    url = "https://github.com/expenses/babble";
-    rev = "abc1bce6efd15225dc013f310343e6866a00a1f1";
-    ref = "nix-test";
-    submodules = true;
-    };
-
-    # Distinguishing between native build inputs (runnable on the host
-    # at compile time) and normal build inputs (runnable on target
-    # platform at run time) is important for cross compilation.
-    nativeBuildInputs = [ cmake ninja ];
-    buildInputs = [ libclang llvm ];
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, pkgs, system, ... }: {
+        packages = {
+          babble = pkgs.callPackage ./package.nix { };
+          default = config.packages.babble;
+
+          x86 = pkgs.pkgsCross.gnu64.callPackage ./package.nix { };
+          x86-static = pkgs.pkgsCross.gnu64.pkgsStatic.callPackage ./package.nix { };
+          aarch64 = pkgs.pkgsCross.aarch64-multiplatform.callPackage ./package.nix { };
+          aarch64-static = pkgs.pkgsCross.aarch64-multiplatform.pkgsStatic.callPackage ./package.nix { };
+
+          windows = pkgs.pkgsCross.mingwW64.callPackage ./package.nix { };
+        };
+      };
+      flake = { };
+    };
 }
