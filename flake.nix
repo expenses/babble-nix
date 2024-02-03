9f14c475a0d0e623329f3fee636b1b8a55b2de3d
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-02-03 20:30:01 +1300

    Formatting

--- flake.nix
+++ flake.nix
@@ -1,22 +1,25 @@
 {
   description = "C++ Crosscompilation Example";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      systems =
+        [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, pkgs, system, ... }: {
         packages = {
           babble = pkgs.callPackage ./package.nix { };
           default = config.packages.babble;
 
           x86 = pkgs.pkgsCross.gnu64.callPackage ./package.nix { };
-          x86-static = pkgs.pkgsCross.gnu64.pkgsStatic.callPackage ./package.nix { };
-          aarch64 = pkgs.pkgsCross.aarch64-multiplatform.callPackage ./package.nix { };
-          aarch64-static = pkgs.pkgsCross.aarch64-multiplatform.pkgsStatic.callPackage ./package.nix { };
+          x86-static =
+            pkgs.pkgsCross.gnu64.pkgsStatic.callPackage ./package.nix { };
+          aarch64 =
+            pkgs.pkgsCross.aarch64-multiplatform.callPackage ./package.nix { };
+          aarch64-static =
+            pkgs.pkgsCross.aarch64-multiplatform.pkgsStatic.callPackage
+            ./package.nix { };
 
           windows = pkgs.pkgsCross.mingwW64.callPackage ./package.nix { };
         };
--- package.nix
+++ package.nix
@@ -1,20 +1,20 @@
 { stdenv, cmake, ninja, libclang, llvm }:
 stdenv.mkDerivation {
-name = "babble";
+  name = "babble";
 
-# good source filtering is important for caching of builds.
-# It's easier when subprojects have their own distinct subfolders.
-src = fetchGit {
-url = "https://github.com/expenses/babble";
-rev = "abc1bce6efd15225dc013f310343e6866a00a1f1";
-ref = "nix-test";
-submodules = true;
-};
+  # good source filtering is important for caching of builds.
+  # It's easier when subprojects have their own distinct subfolders.
+  src = fetchGit {
+    url = "https://github.com/expenses/babble";
+    rev = "abc1bce6efd15225dc013f310343e6866a00a1f1";
+    ref = "nix-test";
+    submodules = true;
+  };
 
-# Distinguishing between native build inputs (runnable on the host
-# at compile time) and normal build inputs (runnable on target
-# platform at run time) is important for cross compilation.
-nativeBuildInputs = [ cmake ninja ];
-buildInputs = [ libclang llvm ];
+  # Distinguishing between native build inputs (runnable on the host
+  # at compile time) and normal build inputs (runnable on target
+  # platform at run time) is important for cross compilation.
+  nativeBuildInputs = [ cmake ninja ];
+  buildInputs = [ libclang llvm ];
 
 }
